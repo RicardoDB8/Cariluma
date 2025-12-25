c89524659c45d5908c767876e7b12010f276dd74
Ricardo Bertarini <bertarini@gmail.com>
2025-12-25 17:03:17 -0300

    Estructura inicial con vistas parciales

--- index.php
+++ index.php
@@ -1,20 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cariluma</title>
-</head>
-<body>
-    <ul>
-        <li><a href="/">Inicio</a></li>
-        <li><a href="/post.php">Publicaciones</a></li>
-        <li><a href="/blog.php">Blog</a></li>
-        <li><a href="/proyectos.php">Proyectos</a></li>
-        <li><a href="/about.php">Sobre mi</a></li>
-        <li><a href="/links.php">Contacto</a></li>
-    </ul>
+<?php
 
-    <h1>Bienvenido a Cariluma</h1>
-</body>
-</html>
+require __DIR__ . '/resources/home.template.php';
---
+++ resources/partials/navbar.php
@@ -0,0 +1,8 @@
+<ul>
+    <li><a href="/">Inicio</a></li>
+    <li><a href="/post.php">Publicaciones</a></li>
+    <li><a href="/blog.php">Blog</a></li>
+    <li><a href="/proyectos.php">Proyectos</a></li>
+    <li><a href="/about.php">Sobre mi</a></li>
+    <li><a href="/links.php">Contacto</a></li>
+</ul>
