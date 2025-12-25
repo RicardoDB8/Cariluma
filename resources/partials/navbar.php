c933adb458b4f9b6542b5fdfcf2c5babae504896
Ricardo Bertarini <bertarini@gmail.com>
2025-12-25 17:30:55 -0300

    Estilos y refactorizacion

--- resources/partials/navbar.php
+++ resources/partials/navbar.php
@@ -1,8 +1,14 @@
-<ul>
-    <li><a href="/">Inicio</a></li>
-    <li><a href="/post.php">Publicaciones</a></li>
-    <li><a href="/blog.php">Blog</a></li>
-    <li><a href="/proyectos.php">Proyectos</a></li>
-    <li><a href="/about.php">Sobre mi</a></li>
-    <li><a href="/links.php">Contacto</a></li>
-</ul>
+<nav class="bg-gray-800">
+    <div class="mx-auto max-w-7xl flex h-16 items-center justify-center">
+        <div class="flex gap-4">
+            <a href="/"          class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium">Inicio</a>
+            <a href="/post.php"  class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Publicaciones</a>
+            <a href="/blog.php" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Blog</a>
+            <a href="/proyectos.php" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Proyectos</a>
+            <a href="/about.php" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Sobre mi</a>
+            <a href="/links.php" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Contacto</a>
+        </div>
+    </div>
+</nav>
+
+
